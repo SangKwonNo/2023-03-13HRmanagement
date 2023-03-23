5a540e574ce9fbc859c834b4bf16baff9d7f8440
gyouns <gyouns76@naver.com>
2023-03-23 18:26:24 +0900

    cococo
    c

--- company_project/src/main/webapp/script/em_salary.js
+++ company_project/src/main/webapp/script/em_salary.js
@@ -28,3 +28,43 @@ function pwCheck() {
 		}
 	});
 }
+
+  function saveAsImage() {
+       /* // 캡처할 화면 영역 지정*/
+        var rect = document.documentElement.getBoundingClientRect();
+        var width = rect.width;
+        var height = rect.height;
+        var x = rect.x;
+        var y = rect.y;
+        
+      /*  // Canvas 생성*/
+        var canvas = document.createElement("canvas");
+        canvas.width = width;
+        canvas.height = height;
+        
+      /*  // Canvas에 캡처된 화면 그리기*/
+        var ctx = canvas.getContext("2d");
+        ctx.drawWindow(window, x, y, width, height, "rgb(255,255,255)");
+        
+      /*  // 이미지 파일로 저장*/
+        var imgData = canvas.toDataURL("image/png");
+        var a = document.createElement("a");
+        a.href = imgData;
+        a.download = "screen.png";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
+
+  /*function saveAsImage() {
+
+    html2canvas(document.body).then(function(canvas) {
+      var imgData = canvas.toDataURL('image/jpeg');
+      
+      var link = document.createElement('a');
+      link.download = 'image.jpg';
+      link.href = imgData;
+      link.click();
+    });
+  }*/
+
